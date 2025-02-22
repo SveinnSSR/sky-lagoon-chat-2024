f355f9ba06984066475fbc0a4882e9d6f4f53a2c
Sveinn <sveinnrafnsson@gmail.com>
2025-02-22 23:58:42 +0000

    Temporarily extended hours for testing in livechat

--- index.js
+++ index.js
@@ -438,7 +438,7 @@ const CONFIDENCE_THRESHOLDS = {
 // LiveChat Constants
 const LIVECHAT_HOURS = {
     START: 9,    // 9 AM
-    END: 23    // 11 PM - Temporarily extended for testing
+    END: 1,    // 1 AM - Temporarily extended for testing
 };
 
 // Transfer trigger patterns
